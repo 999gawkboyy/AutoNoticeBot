93eb703db249262b19fdca0aa787d53690b6040a
= <=>
2024-03-28 18:43:21 +0900

    주말 예외 처리

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ chatroom_name = '23년 도제반'
 def main() -> None:
     open_chatroom(chatroom_name) 
     dDay = calculate_dday()
-    lunchMenu = '\n'.join(findTodaysLunch())
-    message = f"""=============================
+    lunchMenu = findTodaysLunch()
+    if lunchMenu == -1: 
+        pass
+    else: 
+        lunchMenu = '\n'.join(lunchMenu)
+
+    lunchMsg = f"오늘의 급식은 \n{lunchMenu} 입니다" if lunchMenu != -1 else "오늘은 급식이 없네요"
+    message = f"""===========================
 정보처리기능사 실기 시험이 {dDay}일 남았습니다.
------------------------------------------------
-오늘의 급식은 \n{lunchMenu} 입니다.
-============================="""
+----------------------------------
+{lunchMsg}
+==========================="""
     kakao_sendtext(chatroom_name, message)
 
 schedule.every().day.at("07:00").do(main)
