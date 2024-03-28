3bd1d12691b02fd25178c0e5d74fe4069e087031
= <=>
2024-03-28 16:44:58 +0900

    로직 변경

--- main.py
+++ main.py
@@ -2,25 +2,24 @@ from noticeTestDay import open_chatroom, kakao_sendtext, calculate_dday
 import datetime
 import time
 from noticeLunchMenu import findTodaysLunch
+import schedule
 
 chatroom_name = '23년 도제반'
 
-if __name__ == '__main__':
-    today = int(datetime.datetime.now().day)
-    tomorrow = today + 1
-    while 1:
-        today = int(datetime.datetime.now().day)
-        if (today == tomorrow):
-            if (int(datetime.datetime.now().hour) == 7):       
-                open_chatroom(chatroom_name) 
-                dDay = calculate_dday()
-                lunchMenu = '\n'.join(findTodaysLunch())
-                message = f"""=============================
+def main() -> None:
+    open_chatroom(chatroom_name) 
+    dDay = calculate_dday()
+    lunchMenu = '\n'.join(findTodaysLunch())
+    message = f"""=============================
 정보처리기능사 실기 시험이 {dDay}일 남았습니다.
 -----------------------------------------------
 오늘의 급식은 \n{lunchMenu} 입니다.
 ============================="""
-                kakao_sendtext(chatroom_name, message)
-                tomorrow = today + 1
-        print(today, tomorrow)
-        time.sleep(60)
+    kakao_sendtext(chatroom_name, message)
+
+schedule.every().day.at("07:00").do(main)
+
+if __name__ == '__main__':
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
